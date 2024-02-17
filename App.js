1724113793dfe5e4246d054382f4082781716775
nagylas <lnagy.priv@gmail.com>
2024-02-17 01:38:24 +0100

    themed + stars

--- App.js
+++ App.js
@@ -2,11 +2,30 @@ import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infrastructure/theme/index";
+
+import { useFonts, Oswald_400Regular } from "@expo-google-fonts/oswald";
+import { Lato_400Regular } from "@expo-google-fonts/lato";
 
 export default function App() {
+  const [oswaldloaded] = useFonts({
+    Oswald_400Regular,
+  });
+
+  const [latoloaded] = useFonts({
+    Lato_400Regular,
+  });
+
+  if (!oswaldloaded || !latoloaded) {
+    return null;
+  }
+
   return (
     <>
-      <RestaurantsScreen />
+      <ThemeProvider theme={theme}>
+        <RestaurantsScreen />
+      </ThemeProvider>
       <ExpoStatusBar style="auto" />
     </>
   );
--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,27 +1,27 @@
 import React from "react";
 import { Searchbar } from "react-native-paper";
-
-import { View, SafeAreaView, StatusBar } from "react-native";
+import { StatusBar, StyleSheet, SafeAreaView, Text, View } from "react-native";
 import styled from "styled-components/native";
 
 import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
 
 const SafeArea = styled(SafeAreaView)`
   flex: 1;
-  margin-top: ${StatusBar.currentHeight}px;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
 `;
 
 const SearchContainer = styled.View`
-  padding: 16px;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.secondary};
 `;
 
 const RestaurantListContainer = styled.View`
   flex: 1;
-  padding: 16px;
-  background-color: blue;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.bg.secondary};
 `;
 
-export const RestaurantsScreen = () => {
+export const RestaurantsScreen = () => (
   <SafeArea>
     <SearchContainer>
       <Searchbar />
@@ -29,5 +29,5 @@ export const RestaurantsScreen = () => {
     <RestaurantListContainer>
       <RestaurantInfoCard />
     </RestaurantListContainer>
-  </SafeArea>;
-};
+  </SafeArea>
+);
